001be1167fd862fde439d5673fe4b86f3fc700b4
Hussain AlSalem <hjirms@gmail.com>
2025-08-04 13:05:01 +0000

    add black tar payment by Ali Dawood

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def main():
     base_concrete_blocks_4 = project.add_expense("Base Concrete Blocks Fees #4", 598, datetime(2025, 7, 30))
     concrete_blocks_top_up = project.add_expense("Concrete Blocks Top Up", 50000, datetime(2025, 7, 31))
     steel_bars_top_up = project.add_expense("Steel Bars Top Up", 50000, datetime(2025, 7, 31))
+    black_tar = project.add_expense("Black Tar", 2196.50, datetime(2025, 8, 4))
 
 
 
@@ -58,7 +59,7 @@ def main():
                                        + 6374 + 5222 + 80000 + 35000 + 100000
                                        + 37651 + 1955 + 20368 + 10580 + 11902.50
                                        + 4036.50 + 1955 + 500 + 174.02 + 255
-                                       + 20000 +20000 + 598 + 50000 + 50000)
+                                       + 20000 +20000 + 598 + 50000 + 50000 + 2196.50)
     mohammed_alhawaj = project.add_partner("Mohammed AlHawaj", 120000)
 
 
@@ -105,6 +106,7 @@ def main():
     project.add_payment(598, datetime(2025, 7, 30), ali_aldawood, base_concrete_blocks_4)
     project.add_payment(50000, datetime(2025, 7, 31), ali_aldawood, concrete_blocks_top_up)
     project.add_payment(50000, datetime(2025, 7, 31), ali_aldawood, steel_bars_top_up)
+    project.add_payment(2196.50, datetime(2025, 8, 4), ali_aldawood, black_tar)
 
 
     # Add a sale
